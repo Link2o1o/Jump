6e65474ec0b7aa58997222047be35073ff0df200
jmisiuk <jonasmisiuk@hotmail.com>
2015-10-29 15:59:21 -0600

    Class creation

---
+++ src/gameMechanics/RunMechanics.java
@@ -0,0 +1,10 @@
+package gameMechanics;
+
+public class RunMechanics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/gameMechanics/SquareBoard.java
+++ src/gameMechanics/SquareBoard.java
@@ -1,5 +1,30 @@
 package gameMechanics;
 
+import java.util.Scanner;
+
 public class SquareBoard {
+
+	char[][] board = new char[5][5];
+
+	public SquareBoard() {
+	}
+
+	public void fillBoard() {
+		for (int a = 0; a < board.length; a++) {
+			for (int b = 0; b < board.length; b++) {
+				board[a][b] = '*';
+
+			}
+		}
+
+		for (int i = 0; i < board.length; i++) {
+
+			System.out.println();
+
+			for (int j = 0; j < board.length; j++) {
+				System.out.print(board[i][j]);
+			}
+		}
+	}
 
 }
