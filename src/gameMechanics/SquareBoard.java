ff9ae51c99a26754f1c5a40a4758e6842bd9ada1
jmisiuk <jonasmisiuk@hotmail.com>
2015-11-14 19:57:44 -0700

    Adds row numbers

--- src/gameMechanics/SquareBoard.java
+++ src/gameMechanics/SquareBoard.java
@@ -12,7 +12,7 @@ public class SquareBoard extends Board {
 	 * Fills the Square board
 	 */
 	public void fillBoard() {
-		
+
 		for (int a = 0; a < board.length; a++) {
 			for (int b = 0; b < board.length; b++) {
 				board[a][b] = new Peg();
@@ -28,16 +28,32 @@ public class SquareBoard extends Board {
 	@Override
 	public void printBoard() {
 
+		// Prints column numbers
+		System.out.print(" 12345");
+
 		for (int i = 0; i < board.length; i++) {
 
 			System.out.println();
+			// Prints row letter
+			if (i == 0)
+				System.out.print("A ");
+			else if (i == 1)
+				System.out.print("B ");
+			else if (i == 2)
+				System.out.print("C ");
+			else if (i == 3)
+				System.out.print("D ");
+			else if (i == 4)
+				System.out.print("E ");
 
 			for (int j = 0; j < board.length; j++) {
 				System.out.print(board[i][j]);
+
 			}
+
 		}
 	}
-	
+
 	/*
 	 * returns the Board Length
 	 */
@@ -45,7 +61,7 @@ public class SquareBoard extends Board {
 	public int getLength() {
 		return SquareBoard.length;
 	}
-	
+
 	/*
 	 * returns the board height
 	 */
@@ -53,8 +69,9 @@ public class SquareBoard extends Board {
 	public int getHeight() {
 		return SquareBoard.height;
 	}
+
 	/*
-	 * returns the board itself  (These are kind of unnecessary)
+	 * returns the board itself (These are kind of unnecessary)
 	 */
 	@Override
 	public Peg[][] getBoard() {
