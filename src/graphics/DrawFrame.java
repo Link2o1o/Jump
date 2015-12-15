622252841c838e3abfb4c0a12585b708a2beeb22
Link2o1o <private1384@live.com>
2015-12-15 15:38:15 -0700

    Adds DrawFrame

---
+++ src/graphics/DrawFrame.java
@@ -0,0 +1,23 @@
+package graphics;
+
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.*;
+
+public class DrawFrame extends JPanel {
+	
+	ImageIcon imageIcon = new ImageIcon("images/Frame.png");
+	Image frame = imageIcon.getImage();
+
+	protected void paintComponent (Graphics g){
+
+		g.drawImage(frame, 0, 0, getWidth(), getHeight(), this);
+
+		
+		
+	}
+	
+	
+}
