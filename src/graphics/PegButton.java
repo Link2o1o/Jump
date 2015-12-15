53f9dd3c2082fba60adc42c6bebc57412f2de653
jmisiuk <jonasmisiuk@hotmail.com>
2015-12-14 21:19:34 -0800

    Adds marbles as pegs

--- src/graphics/PegButton.java
+++ src/graphics/PegButton.java
@@ -1,19 +1,21 @@
 package graphics;
 
-import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
 @SuppressWarnings("serial")
 public class PegButton extends JButton {
 
+	ImageIcon imageIcon = new ImageIcon("images/RedMarble.png");
+	Image marble = imageIcon.getImage();
+
 	@Override
 	protected void paintComponent(Graphics g) {
-		g.setColor(Color.WHITE);
-		g.fillRect(0, 0, getWidth(), getHeight());
-		g.setColor(Color.BLACK);
-		g.drawOval(0, 0,(int)(getWidth()*.98), (int)(getHeight()*.98));
-		g.setColor(Color.BLUE);
-		g.fillOval(0, 0,(int)(getWidth()*.98), (int)(getHeight()*.98));
-	}
+
+		super.paintComponent(g);
+
+		g.drawImage(marble, 0, 0, getWidth(), getHeight(), this);
+}
 }
